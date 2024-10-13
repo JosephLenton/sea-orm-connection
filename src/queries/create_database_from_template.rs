e6013f5c12e76a28597441d0c528e9ca619009f7
josephlenton@gmail.com <josephlenton@gmail.com>
2024-10-13 01:55:31 +0100

    fix: query_create_database_from_template

--- src/queries/create_database_from_template.rs
+++ src/queries/create_database_from_template.rs
@@ -46,7 +46,7 @@ fn create_database_statement_from_template(
 
     let statement = match db_backend {
         DbBackend::Postgres => {
-            let raw_sql = format!(r#"CREATE DATABASE "{db_name}" TEMPLATE {db_template_name}"#);
+            let raw_sql = format!(r#"CREATE DATABASE "{db_name}" TEMPLATE "{db_template_name}""#);
             let statement = Statement::from_string(db_backend, raw_sql);
 
             statement
@@ -57,4 +57,24 @@ fn create_database_statement_from_template(
     };
 
     Ok(statement)
+}
+
+#[cfg(test)]
+mod query_create_database_from_template {
+    use crate::new_database_connection;
+    use crate::queries::query_create_random_database;
+    use crate::queries::query_create_random_database_from_template;
+
+    const POSTGRES_LOCAL_DB_URL: &'static str = &"postgres://user:password@localhost:5432/tea-orm";
+
+    #[tokio::test]
+    async fn it_should_clone_db() {
+        let db_connection = new_database_connection(POSTGRES_LOCAL_DB_URL.to_string())
+            .await
+            .unwrap();
+        let db_name = query_create_random_database(&db_connection).await.unwrap();
+        let result = query_create_random_database_from_template(&db_connection, &db_name).await;
+
+        assert!(result.is_ok());
+    }
 }
